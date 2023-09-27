d685cdb602b0fa02af621eef277330a3b291dd9e
xuanbao <126218297+wangyuliu@users.noreply.github.com>
2023-09-27 15:01:31 +0800

    Add files via upload

---
+++ RAX/ModelTrainAndPredict/SourceScanner/missions.py
@@ -0,0 +1,3 @@
+root_dir = "E:/kuokuokuo/"
+# E:/kuodashujuji/stage3E:\kuodashujuji\stage3\strace
+sub_dirs = ['pcl-master']
---
+++ RAX/ModelTrainAndPredict/SourceScanner/settings.py
@@ -0,0 +1,6 @@
+INTRINSIC_PATH = "./SourceScanner/data/intrinsics.json"
+BUILTIN_PATH = "./SourceScanner/data/x86-builtins-names.txt"
+X86_MACROS = "./SourceScanner/data/x86-macros.txt"
+SYSCALL_PATH = "./SourceScanner/data/x86-syscall.txt"
+SAVE_PATH_1 = "./SourceScanner/data/result/7.csv"  # 保留数值类型，做成向量
+SAVE_PATH_2 = "./SourceScanner/data/result/8.csv"  # 保留文本类型，做成文本
